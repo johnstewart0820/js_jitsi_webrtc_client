e141b9d18b4a0a6fd279d72d48ea9af04602641b
johnstewart0820 <john.s@outsource.codes>
2020-12-16 17:17:07 +0200

    add: SSO initial commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('wordpress')->redirect();
+});
+
+Route::get('/api/auth/callback', function () {
+    $user = Socialite::driver('wordpress')->user();
+    // echo json_encode($user);
+});
